d0c2842755a789ed3fafa7efb84856768e79840e
Mike <scarlettheadliner@gmail.com>
2026-01-20 18:34:20 +0300

    Add files via upload

--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,15 @@
+
 import json
 import logging
 import pandas as pd
 
 logger = logging.getLogger(__name__)
 
-
 def load_transactions(path: str) -> pd.DataFrame:
     logger.info("Loading Excel")
     df = pd.read_excel(path)
     df["Дата операции"] = pd.to_datetime(df["Дата операции"], errors="coerce")
     return df
-
 
 def dataframe_to_json(df: pd.DataFrame) -> str:
     return json.dumps(df.to_dict(orient="records"), ensure_ascii=False)
